8cca33629ce7dfaa975cb9dd6148909bf8876c21
Alexandra Govorkova <al.govorkova@gmail.com>
2015-03-27 13:19:04 +0300

    Add correct accessing with this to the properties

--- linked-list/DoublyLinkedList.js
+++ linked-list/DoublyLinkedList.js
@@ -4,17 +4,17 @@ function List() {
 	this.tailItem = null; //the last item
 
 	this.head = function head() {
-		if (headItem == null) return null;
-		return headItem.value;
+		if (this.headItem == null) return null;
+		return this.headItem.value;
 	}
 
 	this.tail = function tail() {
-		if (tailItem == null) return null;
-		return tailItem.value;
+		if (this.tailItem == null) return null;
+		return this.tailItem.value;
 	}
 
-	this.getLenght = function getLenght() {
-		return length;
+	this.getLength = function getLength() {
+		return this.length;
 	}
 
 	/* add new item to the end of the list*/
@@ -25,54 +25,54 @@ function List() {
 			previous: null,
 		}
 
-		if (length > 0) {
-			tailItem.next = item;
-			item.previous = tailItem;
+		if (this.length > 0) {
+			this.tailItem.next = item;
+			item.previous = this.tailItem;
 		}
 
-		tailItem = item;
+		this.tailItem = item;
 
-		if (length == 0) {
-			headItem = item;
+		if (this.length == 0) {
+			this.headItem = item;
 		}
 
-		++length;
+		++this.length;
 		return this;		
 	}
 
 	/*deletes item by index; error handling*/
 	this.deleteAt = function deleteAt(index) {
-		if(index >= length || index < 0) {
+		if(index >= this.length || index < 0) {
 			throw new Error("Index is out of the range");
 		}
 
-		if (length == 1) {
-			headItem = null;
-			tailItem = null;
+		if (this.length == 1) {
+			this.headItem = null;
+			this.tailItem = null;
 		}
 		else {
-			var item = headItem;
+			var item = this.headItem;
 			for (var i = 0; i < index; i++) {
 				item = item.next;
 			};
 			if (item.previous) item.previous.next = item.next;
 			if (item.next) item.next.previous = item.previous;
-			if (index == 0) headItem = item.next;
-			if (index == (length - 1)) tailItem = item.previous;
+			if (index == 0) this.headItem = item.next;
+			if (index == (this.length - 1)) this.tailItem = item.previous;
 
 		}
 
-		--length;
+		--this.length;
 		return this;
 	}
 
 	/*returns item by index; error handling*/
 	this.at = function at(index) {
-		if(index >= length || index < 0) {
+		if(index >= this.length || index < 0) {
 			throw new Error("Index is out of the range");
 		}
 
-		var item = headItem;
+		var item = this.headItem;
 		for (var i = 0; i < index; i++) {
 			item = item.next;
 		};
@@ -81,11 +81,11 @@ function List() {
 
 	/*inserts item by index; new item should be placed at the specified index*/
 	this.insertAt = function insertAt(index, val) {
-		if(index > length || index < 0) {
+		if(index > this.length || index < 0) {
 			throw new Error("Index is out of the range");
 		}
 
-		if (index == length) {
+		if (index == this.length) {
 			this.append(val);
 			return this;
 		}
@@ -93,16 +93,16 @@ function List() {
 		if (index == 0) {
 			var item = {
 				value: val, 
-				next: headItem,
+				next: this.headItem,
 				previous: null,
 			}
-			headItem.previous = item;
-			headItem = item;
-			length++;
+			this.headItem.previous = item;
+			this.headItem = item;
+			this.length++;
 			return this;
 		}
 
-		var current = headItem;
+		var current = this.headItem;
 		for (var i = 1; i < index; i++) {
 			current = current.next;
 		};
@@ -115,31 +115,31 @@ function List() {
 
 		current.next.previous = item;
 		current.next = item;
-		length++;
+		this.length++;
 		
 		return this;
 	}
 
 	/*rearranges the list's items back-to-front*/
 	this.reverse = function reverse() {
-		var item = tailItem;
-		for (var i = 0; i < length; i++) {
+		var item = this.tailItem;
+		for (var i = 0; i < this.length; i++) {
 			var buf = item.next;
 			item.next = item.previous;
 			item.previous = buf;
 			item = item.next;
 		}
-		var buf = tailItem;
-		tailItem = headItem;
-		headItem = buf;
+		var buf = this.tailItem;
+		this.tailItem = this.headItem;
+		this.headItem = buf;
 
 		return this;
 	}
 
 	/*applies specified function to each item of the list*/
 	this.each = function each(funct) {
-		var item = headItem;
-		for (var i = 0; i < length; i++) {
+		var item = this.headItem;
+		for (var i = 0; i < this.length; i++) {
 			var val = funct(item.value);
 			//if funct() returns new value write it into the list
 			if (val) item.value = val;
@@ -151,8 +151,8 @@ function List() {
 
 	/*return index of the specified item (first entry)*/
 	this.indexOF = function indexOF(val) {
-		var item = headItem;
-		for (var i = 0; i < length; i++) {
+		var item = this.headItem;
+		for (var i = 0; i < this.length; i++) {
 			if (item.value == val) return i;
 			item = item.next;
 		};
@@ -162,10 +162,10 @@ function List() {
 
 	/*Print the list. Just for testing.*/
 	this.printList = function printList() {
-		if (length == 0) console.log("No items");
+		if (this.length == 0) console.log("No items");
 		else {
-			var item = headItem;
-			for (var i = 0; i < length; i++) {
+			var item = this.headItem;
+			for (var i = 0; i < this.length; i++) {
 				console.log(i + ": " + item.value);
 				item = item.next;
 			}
@@ -174,10 +174,10 @@ function List() {
 
 	/*Reverse print. Just for testing too.*/
 	this.printRev = function printRev() {
-		if (length == 0) console.log("No items");
+		if (this.length == 0) console.log("No items");
 		else {
-			var item = tailItem;
-			for (var i = length - 1; i >= 0; i--) {
+			var item = this.tailItem;
+			for (var i = this.length - 1; i >= 0; i--) {
 				console.log(i + ": " + item.value);
 				item = item.previous;
 			}
