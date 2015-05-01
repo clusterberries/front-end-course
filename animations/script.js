19d4bb05bb47ee68e38b068cd21c243aa7af6a97
Alexandra Govorkova <al.govorkova@gmail.com>
2015-05-02 00:32:36 +0300

    Test the toasts and add the extra task with icons

--- animations/script.js
+++ animations/script.js
@@ -1,4 +1,8 @@
-var button = document.getElementById('addButton');
+var addButton = document.getElementById('addButton');
+var showButton = document.getElementById('showButton');
+var icons = document.getElementById('iconsContainer');
+
+/* Toasts animation */
 var toasts = document.getElementById('toastsContainer');
 var messages = ['Error! Something is broken!', 
     'Success! Everything is under control.', 
@@ -14,7 +18,7 @@ var createToast = function() {
     toasts.appendChild(newToast);
 }
 
-button.addEventListener('click', createToast);
+addButton.addEventListener('click', createToast);
 
 // create a new toast after 2.5s
 setInterval(createToast, 2500); 
@@ -24,3 +28,27 @@ toasts.addEventListener('webkitAnimationEnd', function(event) {
     if (event.animationName === 'disappear')
         event.target.remove();
 });
+
+toasts.addEventListener('animationend', function(event) {
+    // remove the toast if it has disappeared
+    if (event.animationName === 'disappear')
+        event.target.remove();
+});
+
+
+/* Icons animation */
+showButton.addEventListener('click', function() {
+    if (showButton.classList.contains('show')) {
+        showButton.classList.remove('show');
+        showButton.classList.add('hide');
+        icons.classList.add('hidden');
+        icons.classList.remove('showed');
+    }
+    else {
+        showButton.classList.remove('hide');
+        showButton.classList.add('show');
+        icons.classList.add('showed');
+        icons.classList.remove('hidden');
+    }  
+});
+
