f5df586f62063cc1fcebb334cda1e141526a9262
Alexandra Govorkova <al.govorkova@gmail.com>
2015-05-31 18:25:35 +0300

    End the very basic implementation

--- iterator/Iterator.js
+++ iterator/Iterator.js
@@ -2,11 +2,13 @@ var Iterator = function(arr, config) {
 	if (arguments.length === 0) throw new Error ("Can't create an empty object");
 	if (!(arr instanceof Array)) throw new TypeError('Incorrect value');
 	if (typeof config !== 'object') config = undefined;
+
 	this.arr = arr;
 	if (config && config.width) {
+		// if the width is bigger than lenght set width equal length
 		this.width = config.width <= this.arr.length ? config.width : this.arr.length;
 	}
-	else this.width = 1;
+	else this.width = 1; // default value
 	this.cyclic = config ? (config.cyclic || false) : false;
 	this.callback = config ? config.callback : undefined;
 	this.currentPosition = 0;
@@ -27,8 +29,10 @@ var Iterator = function(arr, config) {
 	}.bind(obj));
 }*/
 
+// current subArray
 Iterator.prototype.current = function() {
 	var subarr = this.arr.slice(this.currentPosition, this.currentPosition + this.width);
+	// if get out of cyclic array go to the beginning
 	if (this.cyclic && this.currentPosition + this.width >= this.arr.length) {
 		subarr = subarr.concat(this.arr.slice(0, (this.currentPosition + this.width) % this.arr.length));
 	}
@@ -44,6 +48,7 @@ Iterator.prototype.jumpTo = function(i) {
 		else {
 			if (i > 0) this.currentPosition = i % this.arr.length;
 			else {
+				// calculate new position when get out of cyclic array
 				this.currentPosition = i - Math.floor(i / this.arr.length) * this.arr.length;
 			}
 		}
@@ -63,7 +68,15 @@ Iterator.prototype.forward = function(n) {
 }
 
 Iterator.prototype.backward = function(n) {
-
+	if (!n || typeof n !== 'number') n = 1;
+	if (this.cyclic || this.currentPosition - n + this.width >= 0 && this.currentPosition - n + this.width < this.arr.length) {
+		this.jumpTo(this.currentPosition - n);
+		return this.current();
+	}
+	else {
+		this.currentPosition = 0;
+		return this.current();
+	}
 }
 
 
