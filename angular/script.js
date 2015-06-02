a5aad0780450be4a10b76b62a530e4f7cafee227
Alexandra Govorkova <al.govorkova@gmail.com>
2015-06-03 00:47:34 +0300

    Angular practice 2

--- angular/script.js
+++ angular/script.js
@@ -1,14 +1,25 @@
-var module = angular.module("mod", []);
+var module = angular.module('mod', []);
 
-module.controller("controller", ['$scope', "$http", function (scope, $http){
+module.controller('controller', ['$scope', '$http', 'filterFilter', function (scope, $http, filterFilter){
+	scope.searchTypes = ['asc', 'des'];
+	scope.sortType = scope.searchTypes[0];
+	scope.limit = 10;
+	scope.filterText = '';
 	scope.search = function() {
 		$http.get('repositories.json').then(function(response) {
-			var resp = response.data.sort(function(a, b) {
+			var resp = response.data;
+			resp = scope.sortType === 'asc' ? resp.sort(function(a, b) {
 				if (a.full_name.toLowerCase() > b.full_name.toLowerCase()) return 1;
 				else if (a.full_name.toLowerCase() === b.full_name.toLowerCase()) return 0;
 				else return -1;
+			})
+			: resp.sort(function(a, b) {
+				if (a.full_name.toLowerCase() < b.full_name.toLowerCase()) return 1;
+				else if (a.full_name.toLowerCase() === b.full_name.toLowerCase()) return 0;
+				else return -1;
 			});
-			scope.repos = resp.slice(0, 10);
+			resp = filterFilter(resp, scope.filterText);
+			scope.repos = resp.slice(0, scope.limit);
 		});
 	}
 
